5b32b4f9c61b54c6a6a58a4e5895a9799aa3d123
Denys Goloborodko <den.od.1911@gmail.com>
2020-03-15 16:55:52 +0200

    added google-font and font-awesome

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ import {BookStoreService} from "./services/BookStoreService";
 import {HashRouter} from "react-router-dom";
 import {App} from "./components/App";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "font-awesome/css/font-awesome.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const bookStoreService = new BookStoreService();
 
