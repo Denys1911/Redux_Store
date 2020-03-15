b4790a588cc6f52695553b7d850ee7be4d0b3028
Denys Goloborodko <den.od.1911@gmail.com>
2020-03-15 22:14:06 +0200

    created ShoppingCartTable component

---
+++ src/components/ShoppingCartTable/ShoppingCartTable.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+import "./ShoppingCartTable.css";
+
+export const ShoppingCartTable = () => {
+    return (
+        <div className="shopping-cart-table">
+            <h2>Your Order</h2>
+            <table className="table table-striped">
+                <thead className="thead-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Item</th>
+                        <th scope="col">Count</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Site Reliability Engineering</td>
+                        <td>2</td>
+                        <td>$40</td>
+                        <td className="btn-group">
+                            <button className="btn btn-outline-danger btn-sm">
+                                <i className="fa fa-trash-o" />
+                            </button>
+                            <button className="btn btn-outline-success btn-sm">
+                                <i className="fa fa-plus-circle" />
+                            </button>
+                            <button className="btn btn-outline-warning btn-sm">
+                                <i className="fa fa-minus-circle" />
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <div className="total">
+                Total: $201
+            </div>
+        </div>
+    );
+};
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import {BooksList} from "../BooksList";
+import {ShoppingCartTable} from "../ShoppingCartTable";
 
 export const HomePage = () => {
     return (
         <div className="home-page">
             <BooksList/>
+            <ShoppingCartTable/>
         </div>
     )
 };
