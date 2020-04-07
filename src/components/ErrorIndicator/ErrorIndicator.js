8b5b3eee286458e08e057afc1b3f3dcd5d76b5ab
Denys Goloborodko <den.od.1911@gmail.com>
2020-04-07 08:32:40 +0300

    added error indicator stylization

--- src/components/ErrorIndicator/ErrorIndicator.js
+++ src/components/ErrorIndicator/ErrorIndicator.js
@@ -4,6 +4,7 @@ import "./ErrorIndicator.css";
 
 export const ErrorIndicator = () => (
     <div className="error-indicator">
-        Error...
+        <i className="fa fa-exclamation-triangle"/>
+        Error occurred, please try again later
     </div>
 );
