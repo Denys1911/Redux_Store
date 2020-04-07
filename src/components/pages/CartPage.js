f98a0e98856e84256a200a0987e6905517f404e1
Denys Goloborodko <den.od.1911@gmail.com>
2020-04-07 08:48:49 +0300

    transferred ShoppingCartTable from HomePage to CartPage

--- src/components/pages/CartPage.js
+++ src/components/pages/CartPage.js
@@ -1,7 +1,8 @@
 import React from "react";
+import {ShoppingCartTable} from "../ShoppingCartTable";
 
 export const CartPage = () => (
     <div className="cart-page">
-        Cart
+        <ShoppingCartTable/>
     </div>
 );
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,10 +1,8 @@
 import React from "react";
 import {BooksList} from "../BooksList";
-import {ShoppingCartTable} from "../ShoppingCartTable";
 
 export const HomePage = () => (
     <div className="home-page">
         <BooksList/>
-        <ShoppingCartTable/>
     </div>
 );
